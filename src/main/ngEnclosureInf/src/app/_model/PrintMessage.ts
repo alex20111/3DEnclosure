58e04c8c508e1caefa83268c1f765f09f44a7087
alex20111 <boudale@yahoo.com>
2021-01-15 23:16:15 -0500

    OOps forgot

---
+++ src/main/ngEnclosureInf/src/app/_model/PrintMessage.ts
@@ -0,0 +1,9 @@
+ export class PrintMessage{
+    date!: Date;
+    finished: boolean = false;
+    started: boolean = false;
+    stoped: boolean = false;
+    hour: number = 0;
+    minute: number = 0;
+    seconds: number = 0;
+}
--- src/main/ngEnclosureInf/src/app/services/fan.service.ts
+++ src/main/ngEnclosureInf/src/app/services/fan.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Message } from '../_model/Message';
 import { Config } from './config.service';
 
@@ -13,7 +13,11 @@ export interface FanSpeed {
 })
 export class FanService {
 
-  constructor(private http: HttpClient) { }
+  private subject = new BehaviorSubject<any>(null);
+
+  constructor(private http: HttpClient) {
+
+   }
 
 
   getFanRmp(): Observable<any> {
@@ -36,6 +40,20 @@ export class FanService {
     return this.http.post<Message>('http://localhost:8080/web/fancontrol/updateExtrFanAuto', cfg);
   }
 
+  sendFanAutoMode(isFanAuto: boolean){
+    let exrt: ExtrFanParam = {
+      fanSpeed: 0,
+      fanIsOnAuto: isFanAuto
+    }
+    this.subject.next(exrt);
+  }
+
+  getIsFanOnAuto(): Observable<any>{
+    return this.subject.asObservable();
+  }
+  resetFanOnAuto(): void{
+    this.subject.next(null);
+  }
 
 }
 
